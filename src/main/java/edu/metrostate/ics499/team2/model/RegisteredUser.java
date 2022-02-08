d6b4d014f8f264654e0ea135f186ce6c2867475b
Andrew Vick <atvick21@gmail.com>
2022-02-07 19:30:58 -0600

    Registered User classes

---
+++ src/main/java/edu/metrostate/ics499/team2/model/RegisteredUser.java
@@ -0,0 +1,63 @@
+package edu.metrostate.ics499.team2.model;
+
+import org.springframework.data.annotation.Id;
+import org.springframework.data.mongodb.core.mapping.Document;
+
+@Document(collection = "registered user")
+public class RegisteredUser {
+	
+	@Id
+	private String userId;
+	
+	private String firstName;
+	private String lastName;
+	private String email;
+	private int highScore;
+	
+	public RegisteredUser(String firstName, String lastName, String email, int highScore) {
+		this.firstName= firstName;
+		this.lastName = lastName;
+		this.email = email;
+		this.highScore = highScore;
+	}
+
+	public String getUserId() {
+		return userId;
+	}
+
+	public void setUserId(String userId) {
+		this.userId = userId;
+	}
+
+	public String getFirstName() {
+		return firstName;
+	}
+
+	public void setFirstName(String firstName) {
+		this.firstName = firstName;
+	}
+
+	public String getLastName() {
+		return lastName;
+	}
+
+	public void setLastName(String lastName) {
+		this.lastName = lastName;
+	}
+
+	public String getEmail() {
+		return email;
+	}
+
+	public void setEmail(String email) {
+		this.email = email;
+	}
+
+	public int getHighScore() {
+		return highScore;
+	}
+
+	public void setHighScore(int highScore) {
+		this.highScore = highScore;
+	}
+}
---
+++ src/main/java/edu/metrostate/ics499/team2/repositories/RegisteredUserRepository.java
@@ -0,0 +1,11 @@
+package edu.metrostate.ics499.team2.repositories;
+
+import org.springframework.data.mongodb.repository.MongoRepository;
+import org.springframework.stereotype.Repository;
+
+import edu.metrostate.ics499.team2.model.RegisteredUser;
+
+@Repository
+public interface RegisteredUserRepository extends MongoRepository<RegisteredUser, String> {
+	RegisteredUser findByUserId(String Id);
+}
