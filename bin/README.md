96ea26cc99bc7100e84558980c43fc2498b92189
unknown <abetom763@gmai.com>
2022-02-08 21:21:22 -0600

    adding my changes

---
+++ bin/README.md
@@ -0,0 +1,18 @@
+#TEAM 2 Project
+
+##What we are building
+We are creating a learning tool for students studying chemistry. This tool will allow a sandbox environment for students to learn/validate chemical equations.
+
+###Features
+- Full periodic table containing:
+ - Element name
+ - Atomic Number
+- Full UI allowing user interactivity of each element
+ - Users can add elements together to form new compounds
+ - Users can validate chemical compounds
+
+###Tech Stack
+The following were leveraged to build this project:
+- Spring Boot 4
+- Angular
+- MongoDB
--- src/main/java/edu/metrostate/ics499/team2/model/Game.java
+++ src/main/java/edu/metrostate/ics499/team2/model/Game.java
@@ -1,53 +1,118 @@
 package edu.metrostate.ics499.team2.model;
 
+
 import org.springframework.data.annotation.Id;
 import org.springframework.data.mongodb.core.mapping.Document;
+
 
 @Document(collection = "game")
 public abstract class Game {
 	
 	@Id
-	private String gameId;
+	private Long GameID;
 	
-	private String question;
-	private String answer;
-	
+	private String questions;
+	private String answers;
+	private String matchingSymbol;
+	private String elementName;
+	private String wordSearch;
+	public Game(String matchingSymbol, String elementName, String wordSearch, String question, String answers) {
+		
+		this.matchingSymbol = matchingSymbol;
+		this.elementName = elementName;
+		this.wordSearch = wordSearch;
+		
+		this.answers = answers;
+		this.questions = questions;
+	}
 	public Game() {
 		super();
 	}
-	
-	public Game(String question, String answer) {
-		super();
-		this.question = question;
-		this.answer = answer;
+	public Long getGameID() {
+		return GameID;
+	}
+	public void setGameID(Long gameID) {
+		GameID = gameID;
+	}
+	public String getMatchingSymbol() {
+		return matchingSymbol;
+	}
+	public void setMatchingSymbol(String matchingSymbol) {
+		this.matchingSymbol = matchingSymbol;
+	}
+	public String getElementName() {
+		return elementName;
+	}
+	public void setElementName(String elementName) {
+		this.elementName = elementName;
+	}
+	public String getWordSearch() {
+		return wordSearch;
+	}
+	public String getQuestions() {
+		return questions;
+	}
+	public void setQuestions(String questions) {
+		this.questions = questions;
+	}
+	public String getAnswers() {
+		return answers;
+	}
+	public void setAnswers(String answers) {
+		this.answers = answers;
+	}
+	public void setWordSearch(String wordSearch) {
+		this.wordSearch = wordSearch;
 	}
 	
-	public String getGameId() {
-		return this.gameId;
+	@Override
+	public int hashCode() {
+		final int prime = 31;
+		int output = 1;
+		output = prime * output + ((answers == null) ? 0 : answers.hashCode());
+		output = prime * output + ((questions == null) ? 0 : questions.hashCode());
+		output = prime * output + ((wordSearch == null) ? 0 : wordSearch.hashCode());
+		output = prime * output + ((elementName == null) ? 0 : elementName.hashCode());
+		output = prime * output + ((matchingSymbol == null) ? 0 : matchingSymbol.hashCode());
+		return output;
+	}
+
+	@Override
+	public boolean equals(Object obj) {
+		if (this == obj)
+			return true;
+		if (obj == null)
+			return false;
+		if (getClass() != obj.getClass())
+			return false;
+		
+		
+		Game other = (Game) obj;
+		if (matchingSymbol == null) {
+			if (other.matchingSymbol != null)
+				return false;
+		} else if (!matchingSymbol.equals(other.matchingSymbol))
+			return false;
+		if ( elementName== null) {
+			if (other.elementName != null)
+				return false;
+		} else if (!elementName.equals(other.elementName))
+			return false;
+		if (wordSearch == null) {
+			if (other.wordSearch != null)
+				return false;
+		} else if (!wordSearch.equals(other.wordSearch))
+			return false;
+		return true;
+	}
+	@Override
+	public String toString() {
+		return "Game, GameID=" + GameID + ", matchingSymbol=" + matchingSymbol + ", elementName=" + elementName
+				+ ", wordSearch=" + wordSearch + "";
 	}
 	
-	public void setGameId(String gameId) {
-		this.gameId = gameId;
-	}
 	
-	public String getQuestion() {
-		return this.question;
-	}
 	
-	public void setQuestion(String question) {
-		this.question = question;
-	}
 	
-	public String getAnswer() {
-		return this.answer;
-	}
-	
-	public void setAnswer(String answer) {
-		this.answer = answer;
-	}
-	
-	public Boolean isEqual(String id) {
-		return this.gameId.equals(id);
-	}
 
 }
