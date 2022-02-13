7d27abee257debf224f9b557f6772d0cad93a37e
calebTree <ande4905@gmail.com>
2022-02-12 22:25:20 -0600

    script way of adding data

---
+++ bin/create_elm_data.sh
@@ -0,0 +1,6 @@
+#! /bin/sh
+
+#mongodb://caleb:******@localhost:27017/chem?authSource=admin
+#mongodb://localhost:27017/chem
+#mongoimport --collection=elms --file=elements.json --jsonArray
+mongoimport elements.json -d chem -c elms --drop --jsonArray
