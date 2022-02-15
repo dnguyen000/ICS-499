3ffc6e158d4070be2264ccf653f87cd1bdd3ff46
unknown <abetom763@gmai.com>
2022-02-14 20:14:03 -0600

    chenges

--- src/main/java/edu/metrostate/ics499/team2/model/Game.java
+++ src/main/java/edu/metrostate/ics499/team2/model/Game.java
@@ -9,18 +9,16 @@ import org.springframework.data.mongodb.core.mapping.Document;
 public abstract class Game {
 	
 	@Id
-	private Long GameID;
+	private Long GameId;
 	
 	private String questions;
 	private String answers;
-	private String matchingSymbol;
-	private String elementName;
-	private String wordSearch;
-	public Game(String matchingSymbol, String elementName, String wordSearch, String question, String answers) {
+//	private String matchingSymbol;
+//	private String elementName;
+//	private String wordSearch;
+	public Game(String matchingSymbol, String elementName) {
 		
-		this.matchingSymbol = matchingSymbol;
-		this.elementName = elementName;
-		this.wordSearch = wordSearch;
+		
 		
 		this.answers = answers;
 		this.questions = questions;
@@ -28,27 +26,27 @@ public abstract class Game {
 	public Game() {
 		super();
 	}
-	public Long getGameID() {
-		return GameID;
+	public Long getGameId() {
+		return GameId;
 	}
-	public void setGameID(Long gameID) {
-		GameID = gameID;
+	public void setGameId(Long gameId) {
+		GameId = gameId;
 	}
-	public String getMatchingSymbol() {
-		return matchingSymbol;
-	}
-	public void setMatchingSymbol(String matchingSymbol) {
-		this.matchingSymbol = matchingSymbol;
-	}
-	public String getElementName() {
-		return elementName;
-	}
-	public void setElementName(String elementName) {
-		this.elementName = elementName;
-	}
-	public String getWordSearch() {
-		return wordSearch;
-	}
+//	public String getMatchingSymbol() {
+//		return matchingSymbol;
+//	}
+//	public void setMatchingSymbol(String matchingSymbol) {
+//		this.matchingSymbol = matchingSymbol;
+//	}
+//	public String getElementName() {
+//		return elementName;
+//	}
+//	public void setElementName(String elementName) {
+//		this.elementName = elementName;
+//	}
+//	public String getWordSearch() {
+//		return wordSearch;
+//	}
 	public String getQuestions() {
 		return questions;
 	}
@@ -61,9 +59,9 @@ public abstract class Game {
 	public void setAnswers(String answers) {
 		this.answers = answers;
 	}
-	public void setWordSearch(String wordSearch) {
-		this.wordSearch = wordSearch;
-	}
+//	public void setWordSearch(String wordSearch) {
+//		this.wordSearch = wordSearch;
+//	}
 	
 	@Override
 	public int hashCode() {
@@ -71,9 +69,9 @@ public abstract class Game {
 		int output = 1;
 		output = prime * output + ((answers == null) ? 0 : answers.hashCode());
 		output = prime * output + ((questions == null) ? 0 : questions.hashCode());
-		output = prime * output + ((wordSearch == null) ? 0 : wordSearch.hashCode());
-		output = prime * output + ((elementName == null) ? 0 : elementName.hashCode());
-		output = prime * output + ((matchingSymbol == null) ? 0 : matchingSymbol.hashCode());
+//		output = prime * output + ((wordSearch == null) ? 0 : wordSearch.hashCode());
+//		output = prime * output + ((elementName == null) ? 0 : elementName.hashCode());
+//		output = prime * output + ((matchingSymbol == null) ? 0 : matchingSymbol.hashCode());
 		return output;
 	}
 
@@ -88,28 +86,27 @@ public abstract class Game {
 		
 		
 		Game other = (Game) obj;
-		if (matchingSymbol == null) {
-			if (other.matchingSymbol != null)
-				return false;
-		} else if (!matchingSymbol.equals(other.matchingSymbol))
+//		if (matchingSymbol == null) {
+//			if (other.matchingSymbol != null)
+//				return false;
+//		} else if (!matchingSymbol.equals(other.matchingSymbol))
+//			return false;
+//		if ( elementName== null) {
+//			if (other.elementName != null)
+//				return false;
+//		} else if (!elementName.equals(other.elementName))
+//			return false;
+//		if (wordSearch == null) {
+//			if (other.wordSearch != null)
+//				return false;
+//		} else if (!wordSearch.equals(other.wordSearch))
 			return false;
-		if ( elementName== null) {
-			if (other.elementName != null)
-				return false;
-		} else if (!elementName.equals(other.elementName))
-			return false;
-		if (wordSearch == null) {
-			if (other.wordSearch != null)
-				return false;
-		} else if (!wordSearch.equals(other.wordSearch))
-			return false;
-		return true;
 	}
 	@Override
 	public String toString() {
-		return "Game, GameID=" + GameID + ", matchingSymbol=" + matchingSymbol + ", elementName=" + elementName
-				+ ", wordSearch=" + wordSearch + "";
+		return "Game [GameID=" + GameId + ", questions=" + questions + ", answers=" + answers + "]";
 	}
+	
 	
 	
 	
---
+++ src/main/java/edu/metrostate/ics499/team2/repositories/QuizeRepository.java
@@ -0,0 +1,13 @@
+//package edu.metrostate.ics499.team2.repositories;
+//
+//
+//import org.springframework.data.mongodb.repository.MongoRepository;
+//import org.springframework.stereotype.Repository;
+//
+//import edu.metrostate.ics499.team2.model.Quize;
+//
+//@Repository
+//public interface QuizeRepository extends MongoRepository<Quize, String>{
+//
+//}
+
