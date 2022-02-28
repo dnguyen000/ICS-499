e287c07b5ca024fcdc1429dda49652daa0591b56
wale0 <mohomudmohomed0@gmail.com>
2022-02-27 18:07:06 -0600

    Molecule Class

--- bin/create_elm_data.sh
+++
@@ -1,6 +0,0 @@
-#! /bin/sh
-
-#mongodb://caleb:******@localhost:27017/chem?authSource=admin
-#mongodb://localhost:27017/chem
-#mongoimport --collection=elms --file=elements.json --jsonArray
-mongoimport elms.json -d chem -c elms --drop --jsonArray
---
+++ src/main/java/edu/metrostate/ics499/team2/controllers/MoleculeController.java
@@ -0,0 +1,30 @@
+package edu.metrostate.ics499.team2.controllers;
+
+import java.util.List;
+
+import edu.metrostate.ics499.team2.model.Flashcard;
+import edu.metrostate.ics499.team2.model.Molecule;
+import edu.metrostate.ics499.team2.repositories.MoleculeRepository;
+
+@RestController
+@RequestMapping("/molecule")
+public class MoleculeController {
+
+	@Autowired
+	private MoleculeRepository moleculeRepo;
+
+	@GetMapping(value = "/all")
+	public List<Molecule> list() {
+		return moleculeRepo.findAll();
+	}
+
+	@GetMapping(value = "{element}")
+	public Molecule getElementBySymbol(String symbol) {
+		return this.moleculeRepo.findBySymbol(symbol);
+	}
+	
+	@PostMapping("/add")
+	public Molecule create(@RequestBody final Molecule element) {
+		return moleculeRepo.save(element);
+	}
+}
