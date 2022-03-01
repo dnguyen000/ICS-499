267aeb8f711813dfd471e6aca7d9c06378db567c
calebTree <ande4905@gmail.com>
2022-03-01 11:40:01 -0600

    dropped in PubChem element JSON ST-69

--- bin/create_elm_data.sh
+++ bin/create_elm_data.sh
@@ -3,4 +3,6 @@
 #mongodb://caleb:******@localhost:27017/chem?authSource=admin
 #mongodb://localhost:27017/chem
 #mongoimport --collection=elms --file=elements.json --jsonArray
-mongoimport elms.json -d chem -c elms --drop --jsonArray
+#mongoimport PubChemElements_all.json  -d chem -c elms --drop --jsonArray
+mongoimport PubChemElements_all.json  -d chem -c elms --drop
+
