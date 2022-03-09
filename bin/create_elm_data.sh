d476fac1be80e8d5452b22a7da49eeea0a2f36b9
calebTree <ande4905@gmail.com>
2022-03-08 19:08:47 -0600

    script to create new Element data from PubChem model

--- bin/create_elm_data.sh
+++ bin/create_elm_data.sh
@@ -3,6 +3,5 @@
 #mongodb://caleb:******@localhost:27017/chem?authSource=admin
 #mongodb://localhost:27017/chem
 #mongoimport --collection=elms --file=elements.json --jsonArray
-#mongoimport PubChemElements_all.json  -d chem -c elms --drop --jsonArray
-mongoimport PubChemElements_all.json  -d chem -c elms --drop
-
+mongoimport new_elms.json  -d chem-lab -c new_elms --drop --jsonArray
+#mongoimport PubChemElements_all.json  -d chem-lab -c elms --drop
