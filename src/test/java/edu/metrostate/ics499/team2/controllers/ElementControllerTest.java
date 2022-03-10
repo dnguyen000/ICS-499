b91fade662b9bf7d4978038308eebf2b6d1d2653
calebTree <ande4905@gmail.com>
2022-03-09 19:47:38 -0600

    renamed ElementRepoTest, created Element controller mock test

---
+++ src/test/java/edu/metrostate/ics499/team2/controllers/ElementControllerTest.java
@@ -0,0 +1,60 @@
+package edu.metrostate.ics499.team2.controllers;
+
+import edu.metrostate.ics499.team2.security.JwtTokenProvider;
+import edu.metrostate.ics499.team2.security.http.JwtAccessDeniedHandler;
+import edu.metrostate.ics499.team2.security.http.JwtAuthenticationEntryPoint;
+import edu.metrostate.ics499.team2.services.ElementService;
+import org.hamcrest.Matchers;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
+import org.springframework.boot.test.mock.mockito.MockBean;
+import org.springframework.boot.web.client.RestTemplateBuilder;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.test.web.servlet.MockMvc;
+import org.springframework.test.web.servlet.request.MockMvcRequestBuilders;
+import org.springframework.test.web.servlet.result.MockMvcResultMatchers;
+import org.springframework.test.web.servlet.setup.MockMvcBuilders;
+import org.springframework.web.context.WebApplicationContext;
+
+import static org.assertj.core.api.AssertionsForClassTypes.assertThat;
+import static org.springframework.test.web.servlet.result.MockMvcResultHandlers.print;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.*;
+
+@WebMvcTest(controllers = ElementController.class)
+public class ElementControllerTest {
+
+    private MockMvc mockMvc;
+    @Autowired
+    private WebApplicationContext webApplicationContext;
+    @MockBean
+    private ElementService elmServiceMock;
+    @MockBean
+    private JwtTokenProvider jwtTokenProvider;
+    @MockBean
+    private JwtAccessDeniedHandler jwtAccessDeniedHandler;
+    @MockBean
+    JwtAuthenticationEntryPoint jwtAuthenticationEntryPoint;
+    @MockBean
+    UserDetailsService userDetailsService;
+    @MockBean
+    RestTemplateBuilder restTemplateBuilder;
+
+    @BeforeEach
+    public void setUp() {
+        mockMvc = MockMvcBuilders.webAppContextSetup(webApplicationContext).build();
+    }
+
+    @Test
+    public void testList() throws Exception {
+        assertThat(this.elmServiceMock).isNotNull();
+        mockMvc.perform(MockMvcRequestBuilders.get("/elements"))
+                .andExpect(status().isOk())
+                .andExpect(content().contentType("text/html;charset=UTF-8"))
+                .andExpect(view().name("elements"))
+                .andExpect(MockMvcResultMatchers.view().name("elements"))
+                .andExpect(content().string(Matchers.containsString("Welcome to Elements Page")))
+                .andDo(print());
+    }
+}
