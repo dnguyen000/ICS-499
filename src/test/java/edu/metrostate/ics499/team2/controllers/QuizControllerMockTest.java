ecae4fb3463315a25cc0c643650799b9525efcd2
calebTree <ande4905@gmail.com>
2022-03-10 15:09:32 -0600

    incoroporated quiz conroller mock test, renamed element and user controller test

--- src/test/java/edu/metrostate/ics499/team2/controllers/QuizControllerMockTest.java
+++ src/test/java/edu/metrostate/ics499/team2/controllers/QuizControllerMockTest.java
@@ -1,18 +1,20 @@
 package edu.metrostate.ics499.team2.controllers;
 
-import edu.metrostate.ics499.team2.model.Mapper;
-import edu.metrostate.ics499.team2.model.RegisteredUser;
+import edu.metrostate.ics499.team2.model.Quiz;
 import edu.metrostate.ics499.team2.security.JwtTokenProvider;
 import edu.metrostate.ics499.team2.security.http.JwtAccessDeniedHandler;
 import edu.metrostate.ics499.team2.security.http.JwtAuthenticationEntryPoint;
-import edu.metrostate.ics499.team2.services.RegisteredUserService;
-import lombok.extern.slf4j.Slf4j;
+import edu.metrostate.ics499.team2.services.QuizService;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.extension.ExtendWith;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
 import org.springframework.boot.test.mock.mockito.MockBean;
+import org.springframework.boot.web.client.RestTemplateBuilder;
+import org.springframework.http.MediaType;
 import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.test.context.junit.jupiter.SpringExtension;
 import org.springframework.test.web.servlet.MockMvc;
 import org.springframework.test.web.servlet.result.MockMvcResultMatchers;
 import org.springframework.test.web.servlet.setup.MockMvcBuilders;
@@ -20,50 +22,53 @@ import org.springframework.web.context.WebApplicationContext;
 
 import java.util.List;
 
+import static org.mockito.Mockito.verify;
 import static org.mockito.Mockito.when;
 import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
 import static org.springframework.test.web.servlet.result.MockMvcResultHandlers.print;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.content;
 
-@Slf4j
-@WebMvcTest(RegisteredUserController.class)
-public class RegisteredUserControllerTest {
-//    @Autowired
+@ExtendWith(SpringExtension.class)
+@WebMvcTest(controllers = QuizController.class)
+public class QuizControllerMockTest {
+
     private MockMvc mockMvc;
     @Autowired
-    private WebApplicationContext context;
+    private WebApplicationContext webApplicationContext;
+
     @MockBean
-    private RegisteredUserService registeredUserService;
+    private QuizService quizServiceMock;
     @MockBean
     private JwtTokenProvider jwtTokenProvider;
     @MockBean
     private JwtAccessDeniedHandler jwtAccessDeniedHandler;
     @MockBean
-    private JwtAuthenticationEntryPoint jwtAuthenticationEntryPoint;
+    JwtAuthenticationEntryPoint jwtAuthenticationEntryPoint;
     @MockBean
-    private UserDetailsService userDetailsService;
+    UserDetailsService userDetailsService;
     @MockBean
-    private Mapper mapper;
+    RestTemplateBuilder restTemplateBuilder;
 
     @BeforeEach
-    public void setup() {
-        this.mockMvc = MockMvcBuilders
-                .webAppContextSetup(this.context)
-//                .apply(springSecurity())
-                .build();
+    public void setUp() {
+        mockMvc = MockMvcBuilders.webAppContextSetup(webApplicationContext).build();
     }
 
     @Test
-    public void shouldReturnAllUsers() throws Exception {
-        when(registeredUserService.getUsers())
-                .thenReturn(List.of(new RegisteredUser("duke", "duke@spring.io")));
+    void shouldReturnAllQuizzes() throws Exception {
+        when(quizServiceMock.list())
+                .thenReturn(List.of(new Quiz("is this correct?", "no")));
         this.mockMvc
-                .perform(get("/user/list"))
+                .perform(get("/quiz/all")
+                .contentType(MediaType.APPLICATION_JSON)
+                .characterEncoding("utf-8"))
                 .andExpect(MockMvcResultMatchers.status().isOk())
                 .andExpect(content().contentType("application/json"))
                 .andExpect(MockMvcResultMatchers.jsonPath("$.size()").value(1))
-                .andExpect(MockMvcResultMatchers.jsonPath("$[0].username").value("duke"))
-                .andExpect(MockMvcResultMatchers.jsonPath("$[0].email").value("duke@spring.io"))
+                .andExpect(MockMvcResultMatchers.jsonPath("$[0].question").value("is this correct?"))
+                .andExpect(MockMvcResultMatchers.jsonPath("$[0].answer").value("no"))
                 .andDo(print());
+        verify(quizServiceMock).list();
     }
+
 }
