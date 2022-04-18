822dee7556cd4b75ae2d39db3945be64deae399f
calebTree <ande4905@gmail.com>
2022-04-17 22:25:41 -0500

    Revert "added csrf filter"
    This reverts commit 64aeed93a371e66737344b74a69106b8aae77d70.

--- src/main/java/edu/metrostate/ics499/team2/security/config/SecurityConfig.java
+++ src/main/java/edu/metrostate/ics499/team2/security/config/SecurityConfig.java
@@ -1,7 +1,6 @@
 package edu.metrostate.ics499.team2.security.config;
 
 import edu.metrostate.ics499.team2.constants.SecurityConstants;
-import edu.metrostate.ics499.team2.security.CustomCsrfFilter;
 import edu.metrostate.ics499.team2.security.JwtAuthorizationFilter;
 import edu.metrostate.ics499.team2.security.http.JwtAccessDeniedHandler;
 import edu.metrostate.ics499.team2.security.http.JwtAuthenticationEntryPoint;
@@ -18,9 +17,6 @@ import org.springframework.security.core.userdetails.UserDetailsService;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.web.authentication.UsernamePasswordAuthenticationFilter;
 import org.springframework.security.web.csrf.CookieCsrfTokenRepository;
-import org.springframework.security.web.csrf.CsrfFilter;
-import org.springframework.security.web.csrf.CsrfTokenRepository;
-import org.springframework.security.web.csrf.HttpSessionCsrfTokenRepository;
 import org.springframework.web.cors.CorsConfiguration;
 import org.springframework.web.cors.CorsConfigurationSource;
 import org.springframework.web.cors.UrlBasedCorsConfigurationSource;
@@ -28,9 +24,6 @@ import org.springframework.web.filter.CorsFilter;
 
 import java.util.Arrays;
 import java.util.Collections;
-
-import static edu.metrostate.ics499.team2.constants.Role.ADMIN;
-import static edu.metrostate.ics499.team2.constants.SecurityConstants.CSRF_IGNORE;
 
 @Configuration
 @EnableWebSecurity
@@ -54,13 +47,14 @@ public class SecurityConfig extends WebSecurityConfigurerAdapter {
     @Override
     protected void configure(HttpSecurity http) throws Exception {
         // https://stackoverflow.com/questions/36261781/x-csrf-token-is-not-generated-by-spring-boot
-        http
+        http.csrf()                                                                         // CSRF settings
+//                .csrfTokenRepository(CookieCsrfTokenRepository.withHttpOnlyFalse())	    // return C(X)SRF-TOKEN cookie for postman
+                .disable()																    // rather not disable CSRF security
+//                .and()
                 .cors().and()
                 .sessionManagement().sessionCreationPolicy(SessionCreationPolicy.STATELESS)     // w/ jwt no need to session
                 .and().authorizeRequests()
-//                .antMatchers("/user/list").hasAnyAuthority(ROLE_ADMIN.getAuthorities())
-                .antMatchers("/user/list").hasRole(ADMIN.name())
-//                .antMatchers("/user/list").hasAnyAuthority("admin")                           // must be admin to access
+//                .antMatchers("/user/users").hasAnyAuthority("admin")                          // must be admin to access
                 .antMatchers(SecurityConstants.PUBLIC_URLS).permitAll()
                 .anyRequest().authenticated()
                 .and()
@@ -68,20 +62,6 @@ public class SecurityConfig extends WebSecurityConfigurerAdapter {
                 .authenticationEntryPoint(jwtAuthenticationEntryPoint)
                 .and()
                 .addFilterBefore(jwtAuthorizationFilter, UsernamePasswordAuthenticationFilter.class);
-
-        http
-                .csrf()                                                                     // CSRF settings
-                .ignoringAntMatchers(CSRF_IGNORE)                                           // URI where CSRF check will not be applied
-                .csrfTokenRepository(csrfTokenRepository())                                 // defines a repository where tokens are stored
-//                .csrfTokenRepository(CookieCsrfTokenRepository.withHttpOnlyFalse())	        // return C(X)SRF-TOKEN cookie for postman
-                .and().addFilterAfter(new CustomCsrfFilter(), CsrfFilter.class);            // Csrf filter in which we will add the cookie
-//                .disable()																  // rather not disable CSRF security
-    }
-
-    private CsrfTokenRepository csrfTokenRepository() {
-        HttpSessionCsrfTokenRepository repository = new HttpSessionCsrfTokenRepository();
-        repository.setHeaderName(CustomCsrfFilter.CSRF_COOKIE_NAME);
-        return repository;
     }
 
     @Override
@@ -102,7 +82,7 @@ public class SecurityConfig extends WebSecurityConfigurerAdapter {
         corsConfiguration.setAllowCredentials(true);
         corsConfiguration.setAllowedOrigins(Collections.singletonList("http://localhost:4200"));
         corsConfiguration.setAllowedHeaders(Arrays.asList("Origin", "Access-Control-Allow-Origin", "Content-Type",
-                "Jwt-Token", "Authorization", "Accept", "X-Requested-With",
+                "Accept", "Jwt-Token", "Authorization", "Accept", "X-Requested-With",
                 "Access-Control-Request-Method", "Access-Control-Request-Headers"));
         corsConfiguration.setExposedHeaders(Arrays.asList("Origin", "Content-Type", "Accept", "Jwt-Token", "Authorization",
                 "Access-Control-Allow-Origin", "Access-Control-Allow-Credentials"));
