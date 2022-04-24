fed06b8c55f3867d4af55ec2ca0e7a06b9d69d40
calebTree <ande4905@gmail.com>
2022-04-24 14:17:41 -0500

    compound attaches userId

--- src/main/java/edu/metrostate/ics499/team2/model/Compound.java
+++ src/main/java/edu/metrostate/ics499/team2/model/Compound.java
@@ -1,72 +1,60 @@
 package edu.metrostate.ics499.team2.model;
 
+import org.springframework.data.mongodb.core.mapping.Document;
+
 import java.util.HashMap;
 import java.util.Map;
 
-import org.springframework.data.annotation.Id;
-import org.springframework.data.mongodb.core.mapping.Document;
-
 @Document(collection = "compound")
 public class Compound {
-	
-	@Id
-	private String id;
-	
-	private final HashMap<String, Integer> elements;
-	private String formula;
-	private String title;
-	private final String userId;
-	
-	public Compound(HashMap<String, Integer> elements, String userId) {
-		this.elements = elements;
-		this.formula = createFormula();
-		this.userId = userId;
-	}
-	
-	private String createFormula() {
-		String formula = "";
-		
-		for(Map.Entry<String, Integer> entry : elements.entrySet()) {
-			String key = entry.getKey();
-			int val = entry.getValue();
-			
-			if (val == 1) {
-				formula += key;
-			}
-			else {
-				formula += key + val;
-			}
-		}
-		
-		return formula;
-	}
-	
-	public HashMap<String, Integer> getElements() {
-		return this.elements;
-	}
-	
-	public String getFormula() {
-		return this.formula;
-	}
 
-	public void setFormula(String formula) {
-		this.formula = formula;
-	}
-	
-	public String getUserId() {
-		return this.userId;
-	}
-	
-	public String getTitle() {
-		return this.title;
-	}
-	
-	public void setTitle(String title) {
-		this.title = title;
-	}
-	
-	public boolean equals(Compound compound) {
-		return this.formula.equals(compound.getFormula()); 
-	}
-	
+    private final HashMap<String, Integer> elements;
+    private final String formula;
+    private String title;
+    private final String userId;
+
+    public Compound(HashMap<String, Integer> elements, String userId) {
+        this.elements = elements;
+        this.formula = createFormula();
+        this.userId = userId;
+    }
+
+    private String createFormula() {
+        StringBuilder formula = new StringBuilder();
+        for (Map.Entry<String, Integer> entry : elements.entrySet()) {
+            String key = entry.getKey();
+            int val = entry.getValue();
+            if (val == 1) {
+                formula.append(key);
+            } else {
+                formula.append(key).append(val);
+            }
+        }
+        return formula.toString();
+    }
+
+    public HashMap<String, Integer> getElements() {
+        return this.elements;
+    }
+
+    public String getFormula() {
+        return this.formula;
+    }
+
+    public String getUserId() {
+        return this.userId;
+    }
+
+    public String getTitle() {
+        return this.title;
+    }
+
+    public void setTitle(String title) {
+        this.title = title;
+    }
+
+    public boolean equals(Compound compound) {
+        return this.formula.equals(compound.getFormula());
+    }
+
 }
--- src/main/java/edu/metrostate/ics499/team2/services/CompoundService.java
+++ src/main/java/edu/metrostate/ics499/team2/services/CompoundService.java
@@ -1,71 +1,75 @@
 package edu.metrostate.ics499.team2.services;
 
-import org.slf4j.Logger;
-import org.slf4j.LoggerFactory;
-import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.stereotype.Service;
-import org.springframework.web.client.HttpStatusCodeException;
-import org.springframework.web.client.RestTemplate;
-//import org.springframework.test.web.client.MockRestServiceServer;
-
+import edu.metrostate.ics499.team2.exceptions.domain.PugApiException;
 import edu.metrostate.ics499.team2.model.Compound;
 import edu.metrostate.ics499.team2.model.PugApiDTO;
 import edu.metrostate.ics499.team2.repositories.CompoundRepository;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.stereotype.Service;
+import org.springframework.web.client.HttpStatusCodeException;
+import org.springframework.web.client.RestTemplate;
 
 import static edu.metrostate.ics499.team2.constants.PugApiConstants.*;
 
 @Service
 public class CompoundService {
-	private final Logger LOG = LoggerFactory.getLogger(getClass());
+    private final Logger LOG = LoggerFactory.getLogger(getClass());
 
-	@Autowired
-	private RestTemplate restTemplate;
-	
-	@Autowired
-	private CompoundRepository compoundRepo;
-	
-	@Autowired
-	private QuizService quizService;
-	
-	
-	/**
-	 * TODO:
-	 * - Add handler for when request to PUG API doesn't return element
-	 * - Add local variable for last request sent (6 second delay if within window of last request)
-	 * - 
-	 * 
-	 */
-	
-	public boolean doesValueExistInRepo(String formula) {
-		return compoundRepo.findCompoundByFormula(formula).size() > 0;
-	}
-	
-	private Compound retrieveCompoundFromRepo(String formula) {
-		LOG.info("Value " + formula + " exists in repo, retrieving...");
-		
-		return compoundRepo.findCompoundByFormula(formula).get(0);
-	}
-	
-	private Compound retrieveCompoundFromPugApi(String formula, Compound compound) {
-		LOG.info("calling PUG API with argument: " + formula);
-		Compound returnValue = null;
-		
-		try {
-			PugApiDTO pugApiValue = restTemplate.getForObject(PUG_PROLOG + PUG_INPUT + formula + PUG_OPERATION + PUG_OUTPUT, PugApiDTO.class);
-			compound.setTitle(pugApiValue.getFirstPropertyTitle());
-			returnValue = compoundRepo.save(compound);
-			
-			quizService.createCompoundQuizes(compound);
-		} catch (HttpStatusCodeException exception) {
-			LOG.error("Received " + exception.getStatusCode().value() + " response code from PUG API.");
-		}
-		
-		return returnValue;
-	}
-	
-	public Compound validateInput(Compound compound) {
-		String formula = compound.getFormula();	
-		
-		return doesValueExistInRepo(formula) ? retrieveCompoundFromRepo(formula) : retrieveCompoundFromPugApi(formula, compound);
-	}
+    @Autowired
+    private RestTemplate restTemplate;
+
+    @Autowired
+    private CompoundRepository compoundRepo;
+
+    @Autowired
+    private QuizService quizService;
+
+
+    /**
+     * TODO:
+     * - Add handler for when request to PUG API doesn't return element
+     * - Add local variable for last request sent (6 second delay if within window of last request)
+     * -
+     */
+
+    public boolean doesValueExistInRepo(String formula) {
+        return compoundRepo.findCompoundByFormula(formula).size() > 0;
+    }
+
+    private Compound retrieveCompoundFromRepo(String formula) {
+        LOG.info("Value " + formula + " exists in repo, retrieving...");
+        return compoundRepo.findCompoundByFormula(formula).get(0);
+    }
+
+    private Compound retrieveCompoundFromPugApi(String formula, Compound compound) throws PugApiException {
+        LOG.info("Calling PUG API with argument: " + formula);
+        String pubChemUrl = PUG_PROLOG + PUG_INPUT + formula + PUG_OPERATION + PUG_OUTPUT;
+        try {
+//            LOG.info("Sending PugAPI url in service: {}", pubChemUrl);
+            PugApiDTO pugApiValue = restTemplate.getForObject(pubChemUrl, PugApiDTO.class);
+            assert pugApiValue != null;
+            compound.setTitle(pugApiValue.getFirstPropertyTitle());
+            if (compound.getUserId() != null) {
+                quizService.createCompoundQuizes(compound);
+                return compoundRepo.save(compound);
+            } else
+                return compound;
+        } catch (HttpStatusCodeException exception) {
+            // to do: parse the JSON returned as an error to get a useful response
+            // LOG.error(exception.getResponseBodyAsString());
+//            LOG.error("Received " + exception.getStatusCode().value() + " response code from PUG API.");
+            if (exception.getStatusCode() == HttpStatus.NOT_FOUND) {
+                throw new PugApiException("404 error from PugAPI url: " + pubChemUrl);
+            } else
+                throw new PugApiException("pug api error");
+        }
+    }
+
+    public Compound validateInput(Compound compound) throws PugApiException {
+        String formula = compound.getFormula();
+        return doesValueExistInRepo(formula) ? retrieveCompoundFromRepo(formula) : retrieveCompoundFromPugApi(formula, compound);
+    }
 }
